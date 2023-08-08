4b7ca13fc8ccd7cfa9d43819436977e4c0de0a5c
RooseveltTech <rooseveltabandy@gmail.com>
2023-08-08 02:40:57 +0100

    backend

--- social_app/settings.py
+++ social_app/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     # 'config.middleware.CorsMiddleware',
@@ -138,6 +139,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = "/static/"
 
