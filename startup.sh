89f6a194acb7aa6beb5ccb215ded652f6e4ca4e7
RooseveltTech <rooseveltabandy@gmail.com>
2023-08-16 11:36:36 +0100

    backend

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 export LANG=C.UTF-8
 
-gunicorn --bind=0.0.0.0 social_app.wsgi & celery -A social_app worker --loglevel=INFO --pool=solo & celery -A social_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+gunicorn --bind=0.0.0.0 social_app.wsgi & celery -A social_app worker --loglevel=INFO --without-gossip --without-mingle --without-heartbeat -Ofair --pool=solo & celery -A social_app beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
