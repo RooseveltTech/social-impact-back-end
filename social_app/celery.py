8731a8f005efb86dc49710d41aabf1b78b7aec56
RooseveltTech <rooseveltabandy@gmail.com>
2023-08-16 01:49:25 +0100

    backend

--- social_app/celery.py
+++ social_app/celery.py
@@ -1,12 +1,12 @@
 import os
 import ssl
 from celery import Celery
-
-# django_url = settings.CELERY_BROKER_URL
+from django.conf import settings
+url = settings.BROKER_URL
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_app.settings')
 
-app = Celery('social_app')
+app = Celery('social_app', broker=f'{url}')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
